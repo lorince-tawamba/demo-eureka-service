b44613294088295bab6a22bae53d47c4f315fe2a
lorincet <lorincet@gmail.com>
2021-12-29 12:29:31 +0100

    Initial commit

---
+++ src/test/java/tech/satic/demo/eurekaservice/DemoEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.satic.demo.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
